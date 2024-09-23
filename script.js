ea32c0a08504b801fa3a4f6427c4ca2fe8f28542
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-09-23 12:47:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
   function exibirAlerta() {
     const mensagemSecreta = "Sua mensagem secreta aqui";
     document.getElementById("texto-secreto").innerHTML = mensagemSecreta;
-}
-
+    document.querySelector("button").classList.add("remove");
+  }
