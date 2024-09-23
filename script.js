53c2c8fe5e621b91985e49b99ade8033b0f185b3
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-09-23 12:45:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
   function exibirAlerta() {
-    alert(" A detetive e sua assistente investigam o sumiço de um livro digital da escola e suspeitam que o culpado pode ser o recém-chegado professor, devido a suas habilidades  em programação.");
-  }
+    const mensagemSecreta = "Sua mensagem secreta aqui";
+    document.getElementById("texto-secreto").innerHTML = mensagemSecreta;
+}
+
