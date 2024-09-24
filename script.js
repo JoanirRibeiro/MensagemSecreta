359ef6282b046f00a148ca1bac8d7a318fdf7777
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-09-24 16:31:59 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function exibirAlerta() {
-    const mensagemSecreta = "Sua mensagem secreta aqui";
+    const mensagemSecreta = "Como não conseguiu as respostas, o aluno removeu o arquivo do servidor para depois trocá-la com o professor";
     const elemento = document.querySelector(".texto");
     elemento.classList.add("texto-secreto");
     elemento.innerHTML = mensagemSecreta;
